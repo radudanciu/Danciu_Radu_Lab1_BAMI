8327ef89bc38aaca43522e414e211f4e7f29e37e
radud <radudanciu1407@yahoo.com>
2025-12-15 18:45:20 +0200

    pct 28,29

---
+++ Danciu_Radu_Lab1_BAMI/Controllers/DanciuRaduController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Danciu_Radu_Lab1_BAMI.Controllers
+{
+    public class DanciuRaduController : Controller
+    {
+        public string Index()
+        {
+            return "Bine ai venit in aplicatia ASP.NET MVC!";
+        }
+
+        public string Salut()
+        {
+            return "Salut!";
+        }
+
+
+        public string MesajPersonalizat(string nume, int varsta)
+        {
+            return $"Salut {nume}! Ai {varsta} ani.";
+        }
+    }
+}
